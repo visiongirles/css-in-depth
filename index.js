1956c2c1291d41356cf7fcc05b2e28d78912f457
Kate Sychenko <busy.sychenko@gmail.com>
2022-11-20 11:00:38 +0300

    include some JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+let selectedColors = ['blue', 'red'];
+selectedColors[4] = 5;
+console.log(selectedColors);
+console.log(selectedColors[3]);
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('Max', 'Sychenko');
+greet('Kate', 'Sychenko');
