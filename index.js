1de6e23c92b188160796010502b30e11b91fdf41
Kate Sychenko <busy.sychenko@gmail.com>
2022-11-20 11:34:25 +0300

    Add comments to index.js and add some buttons functionality to About page

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// Example of array
 let selectedColors = ['blue', 'red'];
 selectedColors[4] = 5;
 console.log(selectedColors);
+
+//Example of calling unspecified value
 console.log(selectedColors[3]);
+
+// Example of funtion
 function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName);
 }
 
+//Example of function's call
 greet('Max', 'Sychenko');
 greet('Kate', 'Sychenko');
 
+// Example of function with parameters
 function square(number) {
     return number * number;
 }
 
+//Example of function's call with arguments
 console.log(square(4));
